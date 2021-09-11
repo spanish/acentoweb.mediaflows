7e87546039ca6fe025b7b11ff157ef4baf565977
Manuel <42007+spanish@users.noreply.github.com>
2021-09-11 15:34:13 +0200

    Test 1

---
+++ src/acentoweb/mediaflows/views/mf_publication_view.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+from acentoweb.mediaflows import _
+from Products.Five.browser import BrowserView
+
+from zc.relation.interfaces import ICatalog
+from collections import OrderedDict
+
+from Acquisition import aq_inner
+from zope.component import getUtility
+from zope.intid.interfaces import IIntIds
+from zope.security import checkPermission
+
+# from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+
+
+class MFPublicationView(BrowserView):
+    # If you want to define a template here, please remove the template from
+    # the configure.zcml registration of this view.
+    # template = ViewPageTemplateFile('mf_activity_view.pt')
+
+    def __call__(self):
+        # Implement your own actions:
+        #self.msg = _(u'A small message')
+        return self.index()
+
+
+    #Both relations'ways' are kept, in case you want to refer 'the other way around later'
+    #see mf_person_view.py
+
+    def get_relatedauthors(self):
+        """Returns authors"""
+        refs = (self.context.relatedAuthors)
+        to_objects = [ref.to_object for ref in refs if not ref.isBroken()]
+        refers = self.get_referers(self.context)
+        from_objects = [ref.from_object for ref in refers if not ref.isBroken()]
+        ref_list = to_objects + from_objects
+        return OrderedDict( (x,1) for x in ref_list ).keys()
+
+    def get_referers(self, context = None):
+        """ Return a list of backreference relationvalues
+        """
+        catalog = getUtility(ICatalog)
+        intids = getUtility(IIntIds)
+        context = context and context or self.context
+        rel_query = { 'to_id' : intids.getId(aq_inner(context)) }
+        rel_items = list(catalog.findRelations(rel_query))
+        return rel_items
